5695b03659f9f3ced3794b3b4619409d030d9355
YohnSuh <yohnsuh@gmail.com>
2023-11-03 14:42:21 +0900

    슬라이드 반응형 처리

--- js/swiper.js
+++ js/swiper.js
@@ -1,15 +1,16 @@
 /* 포트폴리오 슬라이드 이미지 부분 */
-var work_img = new Swiper('.work-img', {
-    // thumbs: {swiper : work_txt},
-    loop : true
+let work_img = new Swiper('.work-img', {
+    slidesPerView : 1,
+    loop: true
 });
 /* 포트폴리오 슬라이드 설명 부분 */
-var work_txt = new Swiper('.work-txt', {
-    // thumbs: {swiper : work_img},
-    loop : true
+let work_txt = new Swiper('.work-txt', {
+    loop : true,
+    effect : 'fade',
+    fadeEffect: { 
+        crossFade: true 
+    }
 });
 
 work_img.controller.control = work_txt;
 work_txt.controller.control = work_img;
-
-
