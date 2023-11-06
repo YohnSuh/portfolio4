4f8f42121bdb2db52cdd28ff89b9532cb24f0fd5
YohnSuh <yohnsuh@gmail.com>
2023-11-06 21:32:11 +0900

    button 수정

---
+++ js/header.js
@@ -0,0 +1,41 @@
+$(window).on('scroll', function() {
+    if($(window).scrollTop()) {
+        $('header').addClass('scroll');
+    } else {
+        $('header').removeClass('scroll');
+    }
+});
+
+/* 메뉴 눌렀을 경우, 해당 페이지로 이동 */
+$('header li').click(function() {
+    let idx = $(this).index();
+    let top = $('section').eq(idx).offset().top;
+    $('html, body').stop().animate({
+        'scrollTop' : top + 'px'
+    });
+});
+
+/* 메뉴 눌렀을 때 li 색상 변경 */
+$(window).scroll(function() {
+    let start = $('body').height();
+    let end = $(window).height();
+
+    let wnd = $(window).scrollTop();
+    let page1 = $('.subPage').offset().top;
+    let page2 = $('.myWork').offset().top - 50;
+    let page3 = start - end - 50;
+    
+    if(wnd >= 0 && wnd < page1) {
+        $('header li').eq(0).addClass('on');
+        $('header li').eq(0).siblings().removeClass('on');
+    } else if(wnd < page2) {
+        $('header li').eq(1).addClass('on');
+        $('header li').eq(1).siblings().removeClass('on');
+    } else if(wnd < page3) {
+        $('header li').removeClass('on');
+        $('header li').eq(2).addClass('on');
+    } else {
+        $('header li').removeClass('on');
+        $('header li').eq(3).addClass('on');
+    }
+});
--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,20 @@
 /* 포트폴리오 슬라이드 이미지 부분 */
 let work_img = new Swiper('.work-img', {
     slidesPerView : 1,
-    loop: true
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable : true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    }
 });
 /* 포트폴리오 슬라이드 설명 부분 */
 let work_txt = new Swiper('.work-txt', {
@@ -9,6 +22,10 @@ let work_txt = new Swiper('.work-txt', {
     effect : 'fade',
     fadeEffect: { 
         crossFade: true 
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
     }
 });
 
